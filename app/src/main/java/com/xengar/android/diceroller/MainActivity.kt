a5b8ad88e304c38a806bd31ca215b5cbe66e86a1
Angel Newton <xanewton@gmail.com>
2019-01-10 17:41:24 -0500

    Make diceImage in a field instead of function variable to avoid expensive call findViewById

--- app/src/main/java/com/xengar/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/xengar/android/diceroller/MainActivity.kt
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
             //Toast.makeText(this, "button clicked!", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -30,7 +32,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage : ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
