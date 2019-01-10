27bcc83329b2d1a5353d0abc8a4fa84779e2ede4
Angel Newton <xanewton@gmail.com>
2019-01-10 17:12:51 -0500

    Change button text by code

--- app/src/main/java/com/xengar/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/xengar/android/diceroller/MainActivity.kt
@@ -2,11 +2,14 @@ package com.xengar.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button : Button = findViewById(R.id.roll_button)
+        button.text = "Let's roll!"
     }
 }
