452763ff0f2e182f5e4dfc13ff7ab423f3a104e3
Angel Newton <xanewton@gmail.com>
2019-01-10 17:22:04 -0500

    Change text when pressing the button

--- app/src/main/java/com/xengar/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/xengar/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.xengar.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button : Button = findViewById(R.id.roll_button)
         button.setOnClickListener {
-            Toast.makeText(this, "button clicked!", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, "button clicked!", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText : TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
     }
 }
